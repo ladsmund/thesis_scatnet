b394f0666b319907d10973873e0cbcf740e1e8bf
Mads <ladsmund@gmail.com>
2016-05-18 12:32:05 +0200

    Use memory mapped array for output data to avoid using too much memory

--- scattconvnet/utilities/dataset/dataset.py
+++ scattconvnet/utilities/dataset/dataset.py
@@ -1,28 +1,31 @@
 import numpy as np
 import json
 import os
+import shutil
+
 
 def numpy_memmap(path, shape, dtype):
     return np.memmap(path, mode='r', shape=shape, dtype=dtype)
+
 
 class Dataset:
     ASSET_EXT = "data"
     META_FILE_EXT = "json"
 
     def __init__(self, path, name=None, meta_file_path=None):
-        self._path = path
+        self.path = path
         self.assets = dict()
         self.name = name
         if name is None:
             self.name = os.path.basename(path).split('.')[0]
         self._meta_file_path = meta_file_path
         if meta_file_path is None:
-            self._meta_file_path = os.path.join(self._path, "%s.%s" % (self.name, self.META_FILE_EXT))
+            self._meta_file_path = os.path.join(self.path, "%s.%s" % (self.name, self.META_FILE_EXT))
 
         if os.path.exists(self._meta_file_path):
             self.load()
-        elif not os.path.exists(self._path):
-            os.mkdir(self._path)
+        elif not os.path.exists(self.path):
+            os.mkdir(self.path)
 
     def save(self, path=None):
         if path is None:
@@ -53,9 +56,15 @@ class Dataset:
 
     def add_asset(self, array, key, generator, version=None, parameters=None, parent_asset=None):
         # Write array to data file
-        data_file = open(os.path.join(self._path, key + ".data"), 'wb')
-        data_file.write(array.tobytes())
-        data_file.close()
+        filename = os.path.join(self.path, key + ".data")
+
+        if isinstance(array, np.memmap) and array.filename == filename:
+            # print "The memory map file was already written to the asset file"
+            array.flush()
+        else:
+            data_file = open(filename, 'wb')
+            data_file.write(array.tobytes())
+            data_file.close()
 
         asset = dict()
         asset['generator'] = generator
@@ -68,5 +77,4 @@ class Dataset:
 
     def asset_file_path(self, key):
         file_name = key + "." + self.ASSET_EXT
-        return os.path.join(self._path, file_name)
-
+        return os.path.join(self.path, file_name)
--- scattconvnet/wavelet_transform.py
+++ scattconvnet/wavelet_transform.py
@@ -5,12 +5,12 @@ import multiprocessing
 from multiprocessing import Pool
 
 import sys
+import os
 import signal
+import tempfile
 
 from wavelet import morlet, gauss_kernel
 from utilities.dataset import Dataset
-
-# TODO: Flush data to disk while processing
 
 DEFAULT_SCALE = 4
 DEFAULT_MAX_DEPTH = 2
@@ -38,6 +38,7 @@ class ScatNet:
         self.max_order = max_order
         self.kernel_layers = self.generate_kernels(self.angles, self.scales)
         self.configs = self.scatter_config(range(self.nangles), range(self.scale), self.max_order)
+        self.out_dim = len(self.configs)
 
         def get_key(c):
             l = len(c)
@@ -49,7 +50,7 @@ class ScatNet:
 
         self.configs.sort(key=get_key)
 
-        self.config_indices = dict(zip(self.configs, range(len(self.configs))))
+        self.config_indices = dict(zip(self.configs, range(self.out_dim)))
         self.blur_kernel = gauss_kernel(self.scales[-1])
         self.downsample_step = 2 ** (self.scale - 1)
 
@@ -79,7 +80,6 @@ class ScatNet:
     def scatt_coefficients(self, responses):
         blur = map(lambda i: conv(i, self.blur_kernel), responses)
         scatt_coeff = map(lambda i: i[::self.downsample_step, ::self.downsample_step], blur)
-
         return np.array(scatt_coeff)
 
     @staticmethod
@@ -108,20 +108,30 @@ class ScatNet:
         return config + config_layer
 
 
-def proc_instantiater(scatnet, status_counter_arg):
+def proc_instantiater(scatnet, status_counter_arg, data_arg, responses_array, coefficients_array):
+    global data
+    data = data_arg
+    global responses
+    responses = responses_array
+    global coefficients
+    coefficients = coefficients_array
     global proc_scatnet
     proc_scatnet = scatnet
     global status_counter
     status_counter = status_counter_arg
 
 
-def proc_worker(img):
+def proc_worker(index):
     global proc_scatnet
     global status_counter
-    res = proc_scatnet.transform(img)
+    global data
+    global responses
+    global coefficients
+    response = proc_scatnet.wavelet_transform(data[index, :, :])
+    coefficients[index, :, :, :] = proc_scatnet.scatt_coefficients(response)
+    responses[index, :, :, :] = response
     with status_counter.get_lock():
         status_counter.value += 1
-    return res
 
 
 def print_status(proc_count, proc_size, start_time):
@@ -134,17 +144,40 @@ def print_status(proc_count, proc_size, start_time):
 
 
 def process_data(data, scale=DEFAULT_SCALE, nangles=DEFAULT_NANGLES, max_depth=DEFAULT_MAX_DEPTH,
-                 multi_process=True, status_callback=print_status, callback_interval=DEFAULT_CALLBACK_INTERVAL):
+                 multi_process=True, status_callback=print_status, callback_interval=DEFAULT_CALLBACK_INTERVAL,
+                 responses_file=None, coefficients_file=None):
     nimages = data.shape[0]
 
     t0 = time()
-
+    print "Instantiate scatnet"
     scatnet = ScatNet(nangles=nangles, scale=scale, max_order=max_depth)
 
+
+    print "Instantiate output arrays"
+    if responses_file is None:
+        responses_file = tempfile.NamedTemporaryFile(mode='w+')
+    elif isinstance(responses_file, str):
+        responses_file = open(responses_file, 'w+')
+    responses = np.memmap(responses_file, mode='w+',
+                          shape=(data.shape[0], scatnet.out_dim, data.shape[1], data.shape[2]),
+                          dtype='float64'
+                          )
+    if coefficients_file is None:
+        coefficients_file = tempfile.NamedTemporaryFile(mode='w+')
+    elif isinstance(coefficients_file, str):
+        coefficients_file = open(coefficients_file, 'w+')
+    coefficients = np.memmap(coefficients_file, mode='w+',
+                             shape=(data.shape[0], scatnet.out_dim, data.shape[1] // scatnet.downsample_step,
+                                    data.shape[2] // scatnet.downsample_step),
+                             dtype='float64'
+                             )
+
     if multi_process:
+        print "Instantiate process pool"
         status_counter = multiprocessing.Value('i', 0)
-        pool = Pool(initializer=proc_instantiater, initargs=(scatnet, status_counter))
-        defer = pool.map_async(proc_worker, data)
+        pool = Pool(initializer=proc_instantiater, initargs=(scatnet, status_counter, data, responses, coefficients))
+        print "Start processing"
+        defer = pool.map_async(proc_worker, range(nimages))
 
         while True:
             status = status_counter.value
@@ -159,14 +192,19 @@ def process_data(data, scale=DEFAULT_SCALE, nangles=DEFAULT_NANGLES, max_depth=D
         scatt_res = map(scatnet.transform, data)
         status_callback(nimages, nimages, t0)
 
-    responses, coefficients = zip(*scatt_res)
-    responses = np.array(responses)
-    coefficients = np.array(coefficients)
+        responses, coefficients = zip(*scatt_res)
+        responses = np.array(responses)
+        coefficients = np.array(coefficients)
+
+    t1 = time()
+    responses.flush()
+    coefficients.flush()
+    print("Flushing took %.0f ms\n" % (1000 * (time() - t1)))
 
     t = (time() - t0) * 1000
     print "Processed %i images in %.0f ms (%.0f ms / image)" % (nimages, t, t / nimages)
 
-    return responses, coefficients, scatnet
+    return responses, coefficients
 
 
 if __name__ == '__main__':
@@ -190,18 +228,19 @@ if __name__ == '__main__':
                   "max_depth": args.maxdepth,
                   "scale": args.scale}
 
-    signal.signal(signal.SIGINT, lambda signum, _: exit(signum))
-
-    responses, coefficients, scatnet = process_data(data=data,
-                                                    scale=args.scale,
-                                                    nangles=args.nangles,
-                                                    max_depth=args.maxdepth,
-                                                    multi_process=args.multi_process)
-
     generator_name = "scattnet"
     parameter_string = "a%02i_s%02i_m%02i" % (args.nangles, args.scale, args.maxdepth)
     responses_key = "%s_%s_%s_resp" % (generator_name, parameter_string, args.asset_key)
     coefficients_key = "%s_%s_%s_coef" % (generator_name, parameter_string, args.asset_key)
+
+    signal.signal(signal.SIGINT, lambda signum, _: exit(signum))
+    responses, coefficients = process_data(data=data,
+                                           scale=args.scale,
+                                           nangles=args.nangles,
+                                           max_depth=args.maxdepth,
+                                           multi_process=args.multi_process,
+                                           responses_file=os.path.join(dataset.path, responses_key + ".data"),
+                                           coefficients_file=os.path.join(dataset.path, coefficients_key + ".data"), )
 
     dataset.add_asset(responses, responses_key, generator=generator_name, parameters=parameters,
                       parent_asset=args.asset_key)
