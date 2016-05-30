6e91f2ad0e8545355bda277e177f9a452b276741
mads <ladsmund@gmail.com>
2016-05-30 17:00:30 +0200

    Instantiate the worker pool locally

--- classifiers/affine_model_multi.py
+++ classifiers/affine_model_multi.py
@@ -5,17 +5,18 @@ import affine_model
 
 class AffineModelPar(affine_model.AffineModel):
     def __init__(self, *argv, **kwargs):
-        self.pool = multiprocessing.Pool()
         affine_model.AffineModel.__init__(self, *argv, **kwargs)
 
     def classify(self, data, dim=None):
+        pool = multiprocessing.Pool()
         distances = np.zeros(shape=(data.shape[0], len(self.model_keys)), dtype='float32')
 
         assync_results = []
         for i, model_key in enumerate(self.model_keys):
             model_path = self.get_model_path(model_key)
-            apply_res = self.pool.apply_async(affine_model.distance, (model_path, data), {'dim': dim})
+            apply_res = pool.apply_async(affine_model.distance, (model_path, data), {'dim': dim})
             assync_results.append((i, apply_res))
+        pool.close()
 
         for i, r in assync_results:
             distances[:, i] = r.get()
